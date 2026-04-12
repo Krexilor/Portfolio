3835a2644ad5c4267796b3c2e04ead7c999a0a36
Krexilor <krexilor@gmail.com>
2026-04-12 13:01:55 +0530

    feat: setup initial sections with placeholder content and global section layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import { useState } from 'react'
 import Cursor from './components/common/Cursor'
 import PreLoader from './components/common/PreLoader'
 
+// SECTIONS ----------------------------------------------------------------------------------------------------------------------------------------|
+import HomeSection from './sections/Home'
+import AboutSection from './sections/About'
+import SkillsSection from './sections/Skills'
+import ContactSection from './sections/Contact'
+import ProjectsSection from './sections/Projects'
+
 // APPLICATION -------------------------------------------------------------------------------------------------------------------------------------|
 function App() {
     const [loading, setLoading] = useState(true)
@@ -19,7 +26,18 @@ function App() {
             
             {/* Main content */}
             <main>
-                
+                {/* Navbar */}
+
+
+                {/* Portfolio sections */}
+                <HomeSection />
+                <AboutSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <ContactSection />
+
+                {/* Footer */}
+
             </main>
         </div>
     )
