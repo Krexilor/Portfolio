330bd344569635645808cd772b0530cbdcbadc80
Krexilor <krexilor@gmail.com>
2026-05-03 00:37:47 +0530

    Remove unused and buggy files and clean up existing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,21 @@
 // LIBRARIES ---------------------------------------------------------------------------------------------------------------------------------------|
 import { useState, useEffect } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
 
 // COMPONENTS --------------------------------------------------------------------------------------------------------------------------------------|
 import Cursor from './components/common/Cursor'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
 import PreLoader from './components/common/PreLoader'
-
-// SECTIONS ----------------------------------------------------------------------------------------------------------------------------------------|
-import HomeSection from './sections/Home'
-import WorkSection from './sections/Work'
-import AboutSection from './sections/About'
-import ContactSection from './sections/Contact'
-import ProcessSection from './sections/Process'
-
-// PAGES --------------------------------------------------------------------------------------------------------------------------------------------|
-import Blog from './pages/Blog'
-import Resume from './pages/Resume'
 
 // APPLICATION -------------------------------------------------------------------------------------------------------------------------------------|
 function App() {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        if (loading) {
-            document.documentElement.style.overflow = 'hidden'
-        } else {
-            document.documentElement.style.overflow = 'auto'
-        }
+        document.documentElement.style.overflow = loading ? 'hidden' : 'auto'
 
         return () => {
             document.documentElement.style.overflow = 'auto'
         }
     }, [loading])
-
-    const location = useLocation()
-    const isDetailPage = location.pathname === '/blog' || location.pathname === '/resume'
 
     return (
         <div className = "min-h-screen bg-black">
@@ -44,37 +23,12 @@ function App() {
             <Cursor isSystemLoading = {loading} />
 
             {/* Loading screen */}
-            {loading && <PreLoader onComplete = {() => setLoading(false)} />}
-            
-            {/* Main content - only render when loading is complete */}
+            {loading && <PreLoader onComplete={() => setLoading(false)} />}
+
+            {/* Main content placeholder */}
             {!loading && (
                 <main>
-                    {!isDetailPage && <Navbar />}
-
-                    <Routes>
-                        <Route path = "/" element = {
-                            <>
-                                <HomeSection />
-                                <AboutSection />
-                                <WorkSection />
-                                <ProcessSection />
-                                <ContactSection />
-                                <Footer />
-                            </>
-                        } />
-                        <Route path = "/blog" element = {<Blog />} />
-                        <Route path = "/resume" element = {<Resume />} />
-                        <Route path = "*" element = {
-                            <>
-                                <HomeSection />
-                                <AboutSection />
-                                <WorkSection />
-                                <ProcessSection />
-                                <ContactSection />
-                                <Footer />
-                            </>
-                        } />
-                    </Routes>
+                    {/* Add your current page or section components here */}
                 </main>
             )}
         </div>
